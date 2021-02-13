ced3f1b99330d51c398d20a9041d245dbaa68ead
pablo <smcgrath777@gmail.com>
2021-02-13 15:39:13 -0800

    fixed life

---
+++ build/precache-manifest.413e3bcc067529a6a9bb1f5f3a76a96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e07cc87c1b1950609f190d1f3d02cc",
+    "url": "https://color-sorter.herokuapp.com/index.html"
+  },
+  {
+    "revision": "2d7553ed758884384d3c",
+    "url": "https://color-sorter.herokuapp.com/static/css/main.fd9d169f.chunk.css"
+  },
+  {
+    "revision": "ba6d735a5ac37f0b3d3f",
+    "url": "https://color-sorter.herokuapp.com/static/js/2.d9ba9887.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "https://color-sorter.herokuapp.com/static/js/2.d9ba9887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d7553ed758884384d3c",
+    "url": "https://color-sorter.herokuapp.com/static/js/main.10ffb145.chunk.js"
+  },
+  {
+    "revision": "2df2ace26f9c88653038",
+    "url": "https://color-sorter.herokuapp.com/static/js/runtime-main.e7495988.js"
+  }
+]);
